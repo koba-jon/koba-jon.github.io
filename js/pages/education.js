884ab2e2a406db18c2a113cf371f248b3e15bae1
Hiroki Kobayashi <56967584+koba-jon@users.noreply.github.com>
2026-03-29 21:35:58 +0900

    Update education.js

--- js/pages/education.js
+++ js/pages/education.js
@@ -4,22 +4,37 @@
 
   try {
     const items = await loadJson('data/education.json');
-    document.getElementById('edu-timeline').innerHTML = items.map((item) => `
-      <article class="edu-tl-item">
-        <div class="edu-tl-dot"></div>
-        <div class="edu-tl-card">
-          <div class="edu-tl-period">${escapeHtml(item.period)}</div>
-          <div class="edu-tl-degree">${escapeHtml(item.degree)}</div>
-          <div class="edu-tl-school">${escapeHtml(item.school)}</div>
-          <div class="edu-tl-divider"></div>
-          <div class="edu-tl-meta">
-            <div class="edu-tl-row"><span class="edu-tl-key">Lab</span><span class="edu-tl-val">${escapeHtml(item.lab)}</span></div>
-            <div class="edu-tl-row"><span class="edu-tl-key">Supervisor</span><span class="edu-tl-val">${escapeHtml(item.supervisor)}</span></div>
-            <div class="edu-tl-row"><span class="edu-tl-key">Dissertation</span><span class="edu-tl-thesis">${escapeHtml(item.dissertation)}</span></div>
+
+    document.getElementById('edu-timeline').innerHTML = items.map((item) => {
+      const metaRows = [
+        item.lab
+          ? `<div class="edu-tl-row"><span class="edu-tl-key">Lab</span><span class="edu-tl-val">${escapeHtml(item.lab)}</span></div>`
+          : '',
+        item.supervisor
+          ? `<div class="edu-tl-row"><span class="edu-tl-key">Supervisor</span><span class="edu-tl-val">${escapeHtml(item.supervisor)}</span></div>`
+          : '',
+        item.dissertation
+          ? `<div class="edu-tl-row"><span class="edu-tl-key">Dissertation</span><span class="edu-tl-thesis">${escapeHtml(item.dissertation)}</span></div>`
+          : ''
+      ].filter(Boolean).join('');
+
+      return `
+        <article class="edu-tl-item">
+          <div class="edu-tl-dot"></div>
+          <div class="edu-tl-card">
+            <div class="edu-tl-period">${escapeHtml(item.period)}</div>
+            <div class="edu-tl-degree">${escapeHtml(item.degree)}</div>
+            <div class="edu-tl-school">${escapeHtml(item.school)}</div>
+            ${
+              metaRows
+                ? `<div class="edu-tl-divider"></div>
+                   <div class="edu-tl-meta">${metaRows}</div>`
+                : ''
+            }
           </div>
-        </div>
-      </article>
-    `).join('');
+        </article>
+      `;
+    }).join('');
   } catch (error) {
     console.warn(error);
   }
